a8bb00e68ab70f8aca13b59afbf3db04294822dd
flexilef <fchanlee@mail.sfsu.edu>
2017-02-08 00:19:28 -0800

    Changed project name to POSTProject in order to use POST for the point of sale terminal object. Moved previous logic in main into POST object. Implemented Manager opening store and initializing catalog functionalities. Added empty Cashier, Customer classes.

--- POSTB/src/Products/ProductReader.java
+++ POSTB/src/Products/ProductReader.java
@@ -8,64 +8,69 @@ import java.util.Scanner;
  * @author Katie Fotion
  */
 public class ProductReader {
-    private Scanner scanner;
-    private ArrayList<ProductSpecification> products;
-    
-    // Constructor method
-    public ProductReader(String productFile) {
-        
-        products = new ArrayList();
-        scanner = new Scanner(productFile);
-        while (this.hasMoreProducts()) {
-            ProductSpecification productSpec = this.getNextProduct();
-            products.add(productSpec);
-        }
-        
+
+  private Scanner scanner;
+  private ArrayList<ProductSpecification> products;
+
+  // Constructor method
+  public ProductReader(String productFile) {
+
+    products = new ArrayList();
+    scanner = new Scanner(productFile);
+    while (this.hasMoreProducts()) {
+      ProductSpecification productSpec = this.getNextProduct();
+      products.add(productSpec);
     }
-    
-    private boolean hasMoreProducts() {
-        return (this.scanner.hasNextLine());
+  }
+
+  public ArrayList<ProductSpecification> getProducts() {
+    return products;
+  }
+
+  public void printInfo() {
+    //loop the arrayList of products to display info about the product
+    for (int i = 0; i < products.size(); i++) {
+      ProductSpecification temp = new ProductSpecification(null, 0, null);
+      temp = products.get(i);
+      System.out.println("Product: " + temp.getProductDesc()
+              + "Price: " + temp.getProductPrice() + " UPC: " + temp.getProductUPC());
+
     }
-    
-    private ProductSpecification getNextProduct() {
-        String line = this.scanner.nextLine();
-                
-        // Access upc 
-        String upc = "";
-        for (int i = 0; i < 4; i++)
-            upc += line.charAt(i);
-                
-        // Access text desription
-        String textDesc = "";
-        for (int i = 9; i < 29; i++) 
-            textDesc += line.charAt(i);
-                
-        // Access price
-        String priceStr = "";
-        for (int i = 34; i < line.length(); i++) 
-            priceStr += line.charAt(i);
-                
-        // Convert price to double
-        double price = Double.parseDouble(priceStr);
-                
-        // Initialize new ProductSpecification object
-        ProductSpecification productSpec = new ProductSpecification(textDesc, price, upc);
-        
-        return productSpec;
-        
-        
+    System.out.println("\nEnd of Product Catalog.");
+  }
+
+  private boolean hasMoreProducts() {
+    return (this.scanner.hasNextLine());
+  }
+
+  private ProductSpecification getNextProduct() {
+    String line = this.scanner.nextLine();
+
+    // Access upc 
+    String upc = "";
+    for (int i = 0; i < 4; i++) {
+      upc += line.charAt(i);
     }
-    public void printInfo()
-    {
-       //loop the arrayList of products to display info about the product
-       for(int i = 0; i < products.size(); i++)
-       {
-            ProductSpecification temp = new ProductSpecification(null, 0, null);
-            temp = products.get(i);
-            System.out.println("Product: " + temp.getProductDesc() 
-               + "Price: " + temp.getProductPrice() + " UPC: " + temp.getProductUPC());
-               
-       }  
-       System.out.println("\nEnd of Product Catalog.");
+
+    // Access text desription
+    String textDesc = "";
+    for (int i = 9; i < 29; i++) {
+      textDesc += line.charAt(i);
     }
- }
+
+    // Access price
+    String priceStr = "";
+    for (int i = 34; i < line.length(); i++) {
+      priceStr += line.charAt(i);
+    }
+
+    // Convert price to double
+    double price = Double.parseDouble(priceStr);
+
+    // Initialize new ProductSpecification object
+    ProductSpecification productSpec = new ProductSpecification(textDesc, price, upc);
+
+    return productSpec;
+
+  }
+}
