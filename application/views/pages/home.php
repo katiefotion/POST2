113f9893b3f608c02379954b85cccf2d54e16dff
Ronald Rieger <rrieger@mail.sfsu.edu>
2017-02-11 21:15:10 -0800

    Home page with team page links created

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,1 +1,26 @@
-<p>Hello World</p>
+
+<div class="container-fluig">
+    <div class="jumbotron">
+        <h1 class="text-center">Software Engineering Class SFSU</h1>
+        <h2 class="text-center">Spring 2017</h2>
+        <h2 class="text-center">Section 02</h2>
+        <h2 class="text-center">Team 10</h2>
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Meet The Team!</h3>
+                </div>
+                <div class="panel-body">
+                        <a href="<?php echo site_url('Amanda'); ?>" class="btn btn-default btn-block" role="button">Amanda Robinson</a>
+                        <a href="<?php echo site_url('Ron'); ?>" class="btn btn-default btn-block" role="button">Ronald Rieger</a>
+                        <a href="<?php echo site_url('Rainier'); ?>" class="btn btn-default btn-block" role="button">Rainier Hui</a>
+                        <a href="<?php echo site_url('Jason'); ?>" class="btn btn-default btn-block" role="button">Jason Bockover</a>
+                        <a href="<?php echo site_url('Priya'); ?>" class="btn btn-default btn-block" role="button">Priya Krishnakumar</a>
+                        <a href="<?php echo site_url('Tony'); ?>" class="btn btn-default btn-block" role="button">Tony Filippo</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
