2ef35e25bd9e3bb5e36806456365d4ba0321c16b
Tony Filippo <tonyfilippo93@gmail.com>
2017-02-14 18:32:15 -0800

    updated profile

--- application/views/pages/Tony.php
+++ application/views/pages/Tony.php
@@ -1,1 +1,13 @@
-Hello, my name is Tony!
+<div class="jumbotron">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<img src="Tony.jpg" class="img-responsive img-circle">
+			</div>
+			<div class="col-sm-8">
+				<H1 class="text-center">Tony Filippo</H1>
+			</div>
+		</div>
+
+	</div>
+</div>
