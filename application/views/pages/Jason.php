ca3a58983ce58a69c64bb496aa0cf96bc43466d1
Jbockover <jasonbockover@yahoo.com>
2017-02-14 22:44:15 -0800

    insert photo and set up about page still looking for root of problem. inserted photo again and am now trying to fix issue with pull command not updating

--- application/views/pages/Jason.php
+++ application/views/pages/Jason.php
@@ -6,7 +6,7 @@
 				<img src="assets/img/jason.JPG" class="img-responsive img-circle">
 			</div>
 			<div class="col-sm-8">
-				<H1 class="text-center">Jason N Bockover</H1>
+				<H1 class="text-center">Jason Bockover</H1>
 			</div>
 		</div>
 
