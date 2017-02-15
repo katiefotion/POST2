1eb0994bdb13bd5e17b3ce73546ed77576ba5177
Jbockover <jasonbockover@yahoo.com>
2017-02-14 18:35:21 -0800

    insert photo and set up about page

---
+++ application/views/pages/jason.php
@@ -0,0 +1,16 @@
+
+<div class="jumbotron">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<img src="assets/img/jason.jpg" class="img-responsive img-circle">
+			</div>
+			<div class="col-sm-8">
+				<H1 class="text-center">Jason Bockover</H1>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+
