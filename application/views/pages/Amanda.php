517562045de572b97002a4c23de91dae4ddddfae
Ronald Rieger <rrieger@email.sfsu.edu>
2017-02-15 10:50:17 -0800

    Updated my about page to meet M0 expectations

--- application/views/pages/Amanda.php
+++ application/views/pages/Amanda.php
@@ -6,7 +6,7 @@
 				<img src="assets/img/amanda_pic.JPG" class="img-responsive img-circle">
 			</div>
 			<div class="col-sm-8">
-				<H1 class="text-center">Amanda Robinsonjakdfahfjashf</H1>
+				<H1 class="text-center">Amanda Robinson</H1>
 			</div>
 		</div>
 
