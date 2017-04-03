39d43b0e39272df990e4a0d32e0ada9ba4876212
Ronald Rieger <rrieger@email.sfsu.edu>
2017-04-03 13:00:47 -0700

    Auto formated source code.  No functional changes made

--- application/views/pages/Amanda.php
+++ application/views/pages/Amanda.php
@@ -1,15 +1,14 @@
 <div class="jumbotron">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-4">
-                            <img src="<?= base_url('assets/img/amanda_pic.JPG');?>" class="img-responsive img-circle">
-			</div>
-			<div class="col-sm-8">
-				<H1 class="text-center">Amanda Robinson</H1>
-			</div>
-		</div>
-
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <img src="<?= base_url('assets/img/amanda_pic.JPG'); ?>" class="img-responsive img-circle">
+            </div>
+            <div class="col-sm-8">
+                <H1 class="text-center">Amanda Robinson</H1>
+            </div>
+        </div>
+    </div>
 </div>
 
 
--- application/views/pages/Ron.php
+++ application/views/pages/Ron.php
@@ -1,16 +1,15 @@
 
 <div class="jumbotron">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-4">
-				<img src="<?= base_url('assets/img/ron.jpg');?>" class="img-responsive img-circle">
-			</div>
-			<div class="col-sm-8">
-				<H1 class="text-center">Ronald Rieger</H1>
-			</div>
-		</div>
-
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <img src="<?= base_url('assets/img/ron.jpg'); ?>" class="img-responsive img-circle">
+            </div>
+            <div class="col-sm-8">
+                <H1 class="text-center">Ronald Rieger</H1>
+            </div>
+        </div>
+    </div>
 </div>
 
 
