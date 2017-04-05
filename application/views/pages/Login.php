ca1959905c0e542a7d64cbe6c97f22fb96d21871
anr96 <anikkole@gmail.com>
2017-04-05 13:06:41 -0700

    login page with php

--- application/views/pages/Login.php
+++ application/views/pages/Login.php
@@ -1,48 +1,42 @@
-<div class="jumbotron">
-	<div class="container">
-		<div class="row">
-				<H1 class="text-center">Login</H1>
-		</div>
-	</div>
-</div>
 
-
-
-<div class="row">
-    <div class="col-sm-6">
-        <div class =" text-center">
-            <h2>ALREADY REGISTERED?</h2>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                EMAIL<br><input type="text" name="email" placeholder="jsmith@mail.sfsu.edu"><br><br>
-                PASSWORD <br><input type="text" name="password"><br><br>
-                <input type="submit">
-            </form>
-            <a href="#" class="btn" role="button">Forgot password?</a>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class =" text-center">
+                <?php
+                    echo "<h2>ALREADY REGISTERED?</h2>";
+                ?>
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                    <label>EMAIL</label><br><input type="text" name="email" placeholder="jsmith@mail.sfsu.edu" required><br><br>
+                    <label>PASSWORD</label> <br><input type="text" name="password" required><br><br>
+                    <input type="submit">
+                </form>
+                <a href="#" class="btn" role="button">Forgot password?</a>
+            </div>
         </div>
-    </div>
     
-    <div class="col-sm-6">
-        <div class =" text-center">
-            <h2>DONT HAVE AN ACCOUNT?</h2>
-            <p>Registration only takes a few moments and 
-                you'll receive the following benefits</p>
-        </div>
-            
-            
-            <div class=" col-sm-3">
-                
+        <div class="col-sm-6">
+            <div class =" text-center">
+                <?php 
+                    echo "<h2>DONT HAVE AN ACCOUNT?</h2>";
+                    echo "<p>Registration only takes a few moments and 
+                         you'll receive the following benefits</p>";
+                ?>   
             </div>
-            <div class="col-sm-9">
-                <li>Post items to sell to SFSU students</li>
-                <li>Access <a href="#">Safe Meeting</a> locations</li>
-                <li>All your details saved for fast posting</li>
-                <li>And more!</li><br>
+                <div class=" col-sm-3"></div>
+                <div class="col-sm-9">
+                    <?php
+                        echo "<li>Post items to sell to SFSU students</li>";
+                        echo "<li>Access <a href=\"#\">Safe Meeting</a> locations</li>";
+                        echo "<li>All your details saved for fast posting</li>";
+                        echo "<li>And more!</li><br>";
+                    ?>
+                </div>
                 
-            </div>
-                
-            <p class = "text-center"><a href="#" style="font-size: large"
+                <p class = "text-center"><a href="#" style="font-size: large"
                     class="btn btn-default" role="button">Register Now</a>
-            </p>    
-        
+                </p>    
+            
+        </div>
     </div>
 </div>
