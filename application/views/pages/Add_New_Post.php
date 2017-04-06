5093a958be13eedfe7d8380561bb63a84b82564d
rain <rain2288@gmail.com>
2017-04-05 19:18:52 -0700

    Updated Add_New_post
    formatted to bootstrap

--- application/views/pages/Add_New_Post.php
+++ application/views/pages/Add_New_Post.php
@@ -1,30 +1,66 @@
-<div class = "container">
-    
-    
+<div class = "container-fluid">
+
+
     <div class = "row">
         <div class = "col-md-6">
-            <?php
-            $this->load->helper('form');
-            
-            echo"<h1>Add New Post:</h1>";
-            echo"Name: \t", form_input('name');
-            echo"</br>Price: \t", form_input('price');
-            //echo"</br>Category:";
-            //$this->load->model('categories_model');
-            $this->load->view('widgets/categoryselect');
-            echo"</br>Description:", form_textarea('description');
-            echo"</br>Picture:", form_upload('photo');
-            echo"</br>Other:";
-            ?>
+            <h2>Add New Post:</h2>
+
+            <form>
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="name" class="form-control" id="name", placeholder="Title">
+                </div>
+
+                <div class="form-group">
+                    <label for="price">Price:</label>
+                    <input type="price" class="form-control" id="price" placeholder="$">
+                </div>
+
+                <div class="form-group">
+                    <label for="category">Category:</label>
+                    <select class="form-control" id="category">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+
+                    <div class="form-group">
+                        <label for="description">Description:</label>
+                        <textarea class="form-control" rows="5" id="description"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="photo">Photo:</label>
+                        <input type="photo" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+                        <small id="fileHelp" class="form-text text-muted">File size must be under 4MB.</small>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="other">Other:</label>
+                    <input type="other" class="form-control" id="other">
+                </div>
+
+            </form>
+
         </div>
+        
         <div class = "col-md-6">
-            <?php
-            echo "<h2><strong>Safe Meeting: </h2>";
-            echo "Choose at least 2 options that are convient for you:</br>";
-            echo "Options:";
             
-            ?>
+            <h2><strong>Safe Meeting: </h2>
+                     <div class="form-group">
+                    <label for="location">Location:</label>
+                    <select class="form-control" id="category", aria-describedby="fileHelp">
+                          <small id="fileHelp" class="form-text text-muted">Choose at least 2 options that are convient for you:</small>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+      
         </div>
+ </div>
+      <div class = "row">
         <div class = "col-md-12">
                <?php
             echo "<h2><strong>TODO: Add CANCEL & ADD buttons</h2>";
