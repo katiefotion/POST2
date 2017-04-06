e47694f65f9720eee30cf73567be13d50db96a80
rain <rain2288@gmail.com>
2017-04-05 19:54:46 -0700

    Updated Add_New_post - added GoogleMaps

--- application/views/pages/Add_New_Post.php
+++ application/views/pages/Add_New_Post.php
@@ -1,5 +1,14 @@
-<div class = "container-fluid">
 
+ <head>
+    <style>
+      #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
+  </head>
+
+<div class = "container-fluid">
 
     <div class = "row">
         <div class = "col-md-6">
@@ -23,6 +32,7 @@
                         <option>Games</option>
                         <option>Furniture</option>
                         <option>Electronics</option>
+                        <option>Other</option>
                     </select>
 
                     <div class="form-group">
@@ -44,29 +54,53 @@
             </form>
 
         </div>
-        
-        <div class = "col-md-6">
-            
-            <h2>Safe Meeting: </h2>
-                     <div class="form-group">
-                    <label for="location">Location:</label>
-                    <select class="form-control" id="category", aria-describedby="fileHelp">
-                          <small id="fileHelp" class="form-text text-muted">Choose at least 2 options that are convient for you:</small>
-                        <option>Ceasar Chavez</option>
-                        <option>Cafe Russo</option>
-                        <option>Library</option>
-                        <option>Quad</option>
-                    </select>
-                    <h2>TODO: Maps go here<h2>
-        </div>
- </div>
-      <div class = "row">
-        <div class = "col-md-12">
-               <?php
-            echo "<h2><strong>TODO: Add CANCEL & ADD buttons</h2>";
-            ?>
-            <a href="#" class="btn btn-default btn-block btn-default" role="button">Cancel</a>
-            <a href="#" class="btn btn-default btn-block btn-wrap" role="button">Add</a>
 
+        <div class = "col-md-6">
+
+            <h2>Safe Meeting: </h2>
+            <div class="form-group">
+                <label for="location">Location:</label>
+                <select class="form-control" id="category", aria-describedby="fileHelp">
+                    <small id="fileHelp" class="form-text text-muted">Choose at least 2 options that are convient for you:</small>
+                    <option>Ceasar Chavez</option>
+                    <option>Cafe Russo</option>
+                    <option>Library</option>
+                    <option>Quad</option>
+                </select>
+                <h2>TODO: Maps go here<h2>
+
+   <h3>My Google Maps Demo</h3>
+    <div id="map"></div>
+    <script>
+      function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCPPBuI9Ok7SqFArQX8RjzG4DP4jfLZABc&callback=initMap">
+    </script>
+
+
+            </div>
+        </div>
     </div>
+                        <div class = "row">
+                            <div class = "col-md-12">
+
+                                <h2><strong>TODO: Add CANCEL & ADD buttons</h2>
+
+                                <a href="#" class="btn btn-default btn-block btn-default" role="button">Cancel</a>
+                                <a href="#" class="btn btn-default btn-block btn-wrap" role="button">Add</a>
+
+                            </div>
+                        </div>
 </div>
+
