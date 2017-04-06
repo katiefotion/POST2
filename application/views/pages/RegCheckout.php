72dae4a3137f5ca3372ab4f9421a742c963f5293
Tony Filippo <tonyfilippo93@gmail.com>
2017-04-05 18:33:17 -0700

    made checkout page

---
+++ application/views/pages/RegCheckout.php
@@ -0,0 +1,22 @@
+<h1>Checkout</h1>
+<div style="align-content: right">
+    <p>Price: </p>
+    <p>Total Price:</p>
+</div>
+<h2>Safe Meeting</h2>
+<p style="color: red">*Required</p>
+<div class="container-fluid">
+    <img src="http://www.sfsu.edu/~sfsumap/graphics/base_map.jpg" class="col-md-6">
+    <select class="col-md-6">
+      <option value="library">Library</option>
+      <option value="mx">Malcolm X</option>
+      <option value="cr">Cafe Rosso</option>
+      <option value="hh">Hensil Hall</option>
+      <option value="sss">SFSU Shuttle Stop</option>
+      <option value="hh">The Village</option>
+      <option value="hh">Other</option>
+    </select>
+    
+</div>
+<button>Place Order</button>
+
