2582a324abec35026078cfd57a1df19ee80fefae
priya <kkrishna@mail.sfsu.edu>
2017-04-09 17:43:57 -0700

    Merge origin/horizontal_prototype into horizontal_prototype
    Conflicts:
    	application/views/widgets/navbar.php

--- application/views/pages/Confirmation.php
+++ application/views/pages/Confirmation.php
@@ -5,17 +5,16 @@
             text-align: center;
 
         }
-        .right {
-            text-align: right;
-        }
+    
     </style>
 </head>
 
-<body>
+<div class="container-fluid">
     <div class="center">
         <h1>Thank you!</h1> 
-        <p>Your purchase has been confirmed, and your message has been sent. </p>
+        <p>Your message has been sent. </p>
         <p1>you will receive an email shortly...</p1>
+
     </div>
-</body>
+</div>
 
--- application/views/pages/postconfirmation.php
+++ application/views/pages/postconfirmation.php
@@ -3,43 +3,43 @@
     <style>
         .center {
             text-align: center;
-
         }
         .right {
             text-align: right;
         }
+
     </style>
 </head>
 
-<body>
+<div class="containter-fluid">
     <div class="center">
         <h1>Thank you for posting!</h1> 
     </div>
-    
+
     <h2>Review</h2>
 
-    <div class = "container">
-        <div class = "row">
-            <div class = "col-md-3">
-                <?php
-                $path = site_url("dbimg/$item->photo");
-                echo "<img src='$path' class='img-responsive'>";
-                ?>
-            </div>
-            <div class = "col-md-7">
-                <?php
-                echo "<h2><strong>Item name: </strong>$item->name</h2>";
-                echo "<h3><strong>Description </strong></h3><br>";
-                echo "<p>$item->description</p>";
-                ?>
-            </div>
-            <div class = "col-md-2">
-                <h3>Price: $<?= number_format($item->price, 2); ?></h3>
-            </div>
+    <div class = "row">
+        <div class = "col-md-3 min-lr-pad">
+            <?php
+            $path = site_url("dbimg/$item->photo");
+            echo "<img src='$path' class='img-responsive'>";
+            ?>
+        </div>
+        <div class = "col-md-7">
+            <?php
+            echo "<h2><strong>Item name: </strong>$item->name</h2>";
+            echo "<h3><strong>Description </strong></h3><br>";
+            echo "<p>$item->description</p>";
+            ?>
+        </div>
+        <div class = "col-md-2">
+            <h3>Price: $<?= number_format($item->price, 2); ?></h3>
         </div>
     </div>
 
-    <p>Review email shortly...</p>
-    <button type="button" class="btn btn-default btn-block">Click here to edit</button>
-</body>
+    <div class="right">
+        <button type="button" class="btn btn-outline-primary">Click here to edit</button>
+    </div>
+</div>
+
 
