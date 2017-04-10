a104b27bc74767a52412145213ae66e107ea9675
rain <rain2288@gmail.com>
2017-04-09 22:02:52 -0700

    Updated Cancel button - Return to home

--- application/views/pages/Add_New_Post.php
+++ application/views/pages/Add_New_Post.php
@@ -34,25 +34,27 @@
                 <div class="form-group">
                     <label for="category">Category:</label>
                     <select class="form-control" id="category">
-                        
-                    <?php foreach ($categories as $cat){
-                         echo  "<option>$cat[category]</option>";
-                    } ?>
-              
+
+                        <?php
+                        foreach ($categories as $cat) {
+                            echo "<option>$cat[category]</option>";
+                        }
+                        ?>
+
                     </select>
 
                     <div class="form-group">
                         <label for="description">Description:</label>
-                        <textarea class="form-control" rows="5" id="description"></textarea>
+                        <textarea class="form-control" rows="5" id="description" name = "description"></textarea>
                     </div>
 
                     <div class="form-group">
                         <label for="photo">Photo:</label>
-                        <input type="file" class="form-control-file" id="photo" aria-describedby="fileHelp">
+                        <input type="file" class="form-control-file" id="photo"  name = "photo" aria-describedby="fileHelp">
                         <small id="fileHelp" class="form-text ">File size must be under 4MB.</small>
                     </div>
                 </div>
-            
+
 
             </form>
 
@@ -63,49 +65,51 @@
             <h2>Safe Meeting: </h2>
             <div class="form-group">
                 <label for="location">Location:</label>
-                <select class="form-control" id="locations", aria-describedby="fileHelp">
-                    <small id="fileHelp" class="form-text text-muted">Choose at least 2 options that are convient for you:</small>
-                    <?php foreach ($locations as $loc){
-                         echo  "<option>$loc[location]</option>";
-                    } ?>
+                <select class="form-control" id="locations" name = "location" aria-describedby="fileHelpLoc">
+                    <?php
+                    foreach ($locations as $loc) {
+                        echo "<option>$loc[location]</option>";
+                    }
+                    ?>
+                    <small id="fileHelpLoc" class="form-text">Choose at least 2 options that are convient for you:</small>
 
                 </select>
                 </br>
-    <div id="map"></div>
-    <script>
+                <div id="map"></div>
+                <script>
         
-      function initMap() {
-        var uluru = {lat: 37.722558, lng: -122.4780799};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 16,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCPPBuI9Ok7SqFArQX8RjzG4DP4jfLZABc&callback=initMap">
-    </script>
-
+                  function initMap() {
+                    var uluru = {lat: 37.722558, lng: -122.4780799};
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                      zoom: 16,
+                      center: uluru
+                    });
+                    var marker = new google.maps.Marker({
+                      position: uluru,
+                      map: map
+                    });
+                  }
+                </script>
+                <script async defer
+                        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCPPBuI9Ok7SqFArQX8RjzG4DP4jfLZABc&callback=initMap">
+                </script>
 
             </div>
         </div>
     </div>
-                        <div class = "row">
-                            <div class = "col-md-5"> </div>
-                            Still need to add href to Cancel Button
-                                                            <div class = "col-md-1"> 
-                       <a href="#" class="btn btn-danger btn-block" role="button">Cancel</a>
-</div>
-                                                            <div class = "col-md-1"> 
+    <div class = "row">
+        <div class = "col-md-5"> </div>
+        Still need to add href to Cancel Button
+        <div class = "col-md-1"> 
+            <a href="./" class="btn btn-danger btn-block" role="button">Cancel</a>
+        </div>
+        <div class = "col-md-1"> 
 
-                                <button type="submit" class="btn btn-success btn-block">Add</button>
-                                </div>
-                             <div class = "col-md-5"> </div>
-                            </div>
-                    
+            <button type="submit" class="btn btn-success btn-block">Add</button>
+ 
+        </div>
+        <div class = "col-md-5"> </div>
+    </div>
+
 </div>
 
