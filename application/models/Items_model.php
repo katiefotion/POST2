f6d9b74053401407abc94426fddec4eb3914104e
Ronald Rieger <rrieger@email.sfsu.edu>
2017-04-10 20:49:11 -0700

    added count function and the ability to return ranged data as pages and page size

--- application/models/Items_model.php
+++ application/models/Items_model.php
@@ -1,19 +1,34 @@
 <?php
+
 class Items_model extends CI_Model {
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
         $this->load->database();
     }
 
-    public function get_items($category_id)
-    {
-        return $this->db->get_where('items_briefdescription_view', array('category_id' => $category_id))->result();
+    public function items_count($category_id = 0){
+        return $this->db->count_all('items');
     }
-    
-    public function get_item($id)
-    {
-        return $this->db->get_where('items', array('id'=> $id))->first_row();
+
+    public function get_items($category_id = 0, $page = -1,$how_many = 10) {
+        
+        if($category_id)
+            $this->db->where (array('category_id' => $category_id));
+        // if $page is -1, get everything.  Otherwise, limit the results
+        if($page != -1)
+            $this->db->limit ($how_many, $page*$how_many);
+        
+        return $this->db->get('items_briefdescription_view')->result();
+        
     }
+
+//    public function get_items($category_id,$page = -1) {
+//        return $this->db->get_where('items_briefdescription_view', array('category_id' => $category_id))->result();
+//    }
+
+    public function get_item($id) {
+        return $this->db->get_where('items', array('id' => $id))->first_row();
+    }
+
 }
