8fd5ad359e2eb1ccdc35d3526e2898ada8c792c4
Tony Filippo <tonyfilippo93@gmail.com>
2017-04-11 11:14:56 -0700

    edits to home and view message

--- application/views/pages/ViewMessages.php
+++ application/views/pages/ViewMessages.php
@@ -1,35 +1,33 @@
 <div class="container-fluid">
-  <h1>Messages</h1>
-  <p>View By:</p>
-  <button type="button" onclick="">Date</button>
-  <button type="button" onclick="">Item</button>
-  <button type="button" onclick="">Messenger</button>
-  <table class="table table-bordered">
-      <thead>
-      <tr>
-        <th></th>
-        <th>Date</th>
-        <th>Message</th>
-        <th>Item</th>
-        <th>Messenger</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td><input type="checkbox" value=""></td>
-        <td>3/14/17</td>
-        <td>I would like to buy that</td>
-        <td>Box of Cigarettes</td>
-        <td>Mike Smith</td>
-      </tr>
-      <tr>
-        <td><input type="checkbox" value=""></td>
-        <td>3/15/17</td>
-        <td>Does it come in blue?</td>
-        <td>Bowling Ball</td>
-        <td>Sally King</td>
-      </tr>
-    </tbody>
-  </table>
-  <button type="button" onclick="">Delete</button>
+    <div class="text-center">
+        <h1>Messages</h1>
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+              <th></th>
+              <th onclick="">Date</th>
+              <th>Message</th>
+              <th onclick="">Item</th>
+              <th onclick="">Messenger</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><input type="checkbox" value=""></td>
+              <td>3/14/17</td>
+              <td>I would like to buy that</td>
+              <td>Box of Cigarettes</td>
+              <td>Mike Smith</td>
+            </tr>
+            <tr>
+              <td><input type="checkbox" value=""></td>
+              <td>3/15/17</td>
+              <td>Does it come in blue?</td>
+              <td>Bowling Ball</td>
+              <td>Sally King</td>
+            </tr>
+          </tbody>
+        </table>
+        <button type="button" onclick="">Delete</button>
+    </div>
 </div>
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,14 +1,12 @@
 <div class="container-fluid min-lr-pad">
     <div class="jumbotron" style="margin-bottom: 0px; background-image: url('<?= base_url('assets/img/students.jpeg'); ?>'); background-size: 100% 100%;">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <img src="<?= base_url('assets/img/gatorsell_lg.png'); ?>" class="img-responsive">
-                <h1 class="home-banner">Coming Soon!</h1>
+                <p class="home-banner">A gateway to shopping satisfaction for all SFSU users</p>
             </div>
-            <div class="col-md-8">
-                <h1 class="text-center home-banner">GatorSell, a website exclusively produced for the advantages of SFSU students, has user perks of selling and buying items.</h1>
-                <button type="button" onclick="">Buy</button>
-                <button type="button" onclick="">Sell</button>
+            <div class="col-md-6">
+                <img src="<?= base_url('assets/img/Shopping-Cart-Icon-3-800px.png'); ?>" class="img-responsive">
             </div>
         </div>
     </div>
