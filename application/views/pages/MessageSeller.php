8476f69cc4e51a9776d65611314a4aff08c1225c
anr96 <anikkole@gmail.com>
2017-04-11 21:56:22 -0700

    added cancel button and made both subject/message fields using bootstrap, also shows the variable the page is expecting to receive ($description) and will use for subject line

--- application/views/pages/MessageSeller.php
+++ application/views/pages/MessageSeller.php
@@ -1,42 +1,45 @@
+<?php
+    //expect to receive the item description for the subject line
+    $description = "";
+?>
 <div class="container-fluid">
-		<div class="row">
-			<H1 class="text-center">Message Seller</H1>
-		</div>
-	
-
-
-<div class="row">
-    <div class="text-center">
-        <p>Message the seller about purchasing the item or about questions pertaining to the product</p>    
+    <div class="row">
+	<H1 class="text-center">Message Seller</H1>
     </div>
-</div>
+	
+    <div class="row">
+        <div class="text-center">
+            <p>Message the seller about purchasing the item or about questions pertaining to the product</p>    
+        </div>
+    </div>
     
-<div class="row ">
-    <form class="form-horizontal" action='' method="POST">
-        <div class="row">
-            <div class="col-lg-3"></div>
-            <div class="col-lg-6 form-group">
-                <label for="subject">SUBJECT</label>
-                <textarea id="message" name="message" rows= "1" cols="90" placeholder="Auto-filled item name"></textarea>
-                <p></p>
-            </div>  
-        </div>
-
-        <div class="row">
-            <div class="col-lg-3"></div>
-            <div class="col-lg-6 form-group">
-                <label for="message">MESSAGE</label>
-                <textarea id="message" name="message" rows= "5" cols="90"></textarea>
-                <p></p>
-                <div class="text-right">
-                    <button class="btn btn-default"><strong>Cancel</strong></button>
-                    <button class="btn btn-default"><strong>Send Message</strong></button>
-                </div>
-                    
-               
-                
+    <div class="row ">
+        <form class="form-horizontal" action='' method="POST">
+            <div class="row">
+                <div class="col-lg-3"></div>
+                <div class="col-lg-6 form-group">
+                    <label for="subject">SUBJECT</label>
+                    <!--need to receive the item name for this -->
+                    <div class="panel panel-default panel-body">
+                        <?php
+                            echo $description;
+                        ?>
+                    </div>
+                </div>  
             </div>
-        </div>
-    </form>
-</div>
+
+            <div class="row">
+                <div class="col-lg-3"></div>
+                <div class="col-lg-6 form-group">
+                    <label for="message">MESSAGE</label>
+                    <textarea class="form-control" rows="5" id="message"></textarea>
+                    <p></p>
+                    <div class="text-right">
+                        <button class="btn btn-danger">Cancel</button>
+                        <button class="btn btn-success">Send Message</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
