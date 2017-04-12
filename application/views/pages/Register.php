105670fe91f5a022c3cabc47542e3e4cd5676ec6
anr96 <anikkole@gmail.com>
2017-04-11 21:33:46 -0700

    added terms and cond. checkbox and a cancel button to register pg

--- application/views/pages/Register.php
+++ application/views/pages/Register.php
@@ -1,58 +1,53 @@
-<div class="container-fluid">          
-        <form class="form-horizontal" action='' method="POST">
-        <div id="legend">
-            <legend class=""><h2>Register</h2></legend>
-        </div>
-            
+<div class="container-fluid">   
+    <div class="col-md-2"></div>
+    <div class="col-md-8 text-center">
         
-        <div class="control-group">
-            <label class="control-label"  for="name">NAME</label>
-            <div class="controls">
+       <h2>Register</h2>
+       <form action='' method="POST">
+            <div class="form-group">
+                <label for="name">NAME</label><br>
                 <input type="text" id="name" name="name" placeholder="John Smith" class="input-xlarge">
                 <p></p>
             </div>
-        </div>
  
-    <div class="control-group">
-        <label class="control-label" for="email">EMAIL</label>
-        <div class="controls">
-            <input type="text" id="email" name="email" placeholder="jsmith@sfsu.edu" class="input-xlarge">
-            <p class="help-block">Please provide your SFSU email</p>
-        </div>
-    </div>
+            <div class="form-group">
+                <label for="email">EMAIL</label><br>
+                <input type="email" id="email" name="email" placeholder="jsmith@sfsu.edu" class="input-xlarge">
+                <p class="help-block">Please provide your SFSU email</p>
+            </div>
  
-    <div class="control-group">
-        <label class="control-label" for="phone">PHONE NUMBER</label>
-        <div class="controls">
-            <input type="phone" id="phone" name="phone" placeholder="(555) 555-5555" class="input-xlarge">
-            <p></p>
-        </div>
-    </div>
+            <div class="form-group">
+                <label for="phone">PHONE NUMBER</label><br>
+                <input type="text" id="phone" name="phone" placeholder="(555) 555-5555" class="input-xlarge">
+                <p></p>
+            </div>
             
-    <div class="control-group">      
-        <label class="control-label" for="password">PASSWORD</label>
-        <div class="controls">
-            <input type="password" id="password" name="password" placeholder="" class="input-xlarge">
-            <p class="help-block">Password should be at least 7 characters</p>
-        </div>
-    </div>
+            <div class="form-group">      
+                <label for="password">PASSWORD</label><br>
+                <input type="password" id="password" name="password" placeholder="" class="input-xlarge">
+                <p class="help-block">At least 7 characters</p>
+            </div>
  
-    <div class="control-group">
-      <!-- Password -->
-      <label class="control-label"  for="confirm_password">CONFIRM PASSWORD</label>
-      <div class="controls">
-        <input type="password" id="password_confirm" name="confirm_password" placeholder="" class="input-xlarge">
-        <p class="help-block">Please confirm password</p>
-      </div>
-    </div>
- 
-    <div class="control-group">
-      <div class="controls">
-        <button class="btn btn-success">Register</button>
-      </div>
-    </div>
-                
-        </form>
+            <div class="control-group">
+            <!-- needs to check that they're the same password -->
+                <label for="confirm_password">CONFIRM PASSWORD</label><br>
+                <input type="password" id="password_confirm" name="confirm_password" placeholder="" class="input-xlarge">
+                <p class="help-block">Please confirm password</p>
+            </div>
     
+           <div class="form-group">
+               <p><input type="checkbox" name="terms" autocomplete="off">
+                   I agree to the <a href="#"> Terms & Conditions</a></p>
+           </div>
+           
+            <div class="form-group">
+                <button class="btn btn-danger">Cancel</button>
+                <button class="btn btn-success">Register</button>
+            </div>
+           
+        </form> 
+    </div>
+    <div class="col-md-2"></div>            
+        
 </div>
 
