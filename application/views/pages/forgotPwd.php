c5e943923c988bef1a1790ea4f140ec9897229af
priya <kkrishna@mail.sfsu.edu>
2017-04-12 20:08:53 -0700

    Added forgot password page and a confirmation page for forgot pwd.

---
+++ application/views/pages/forgotPwd.php
@@ -0,0 +1,17 @@
+<div class="container-fluid">
+    <div class="text-center">
+        <h2>Forgot Password?</h2>
+    </div>
+    <form>
+        <div class="form-group">
+            <label for="email">Enter your email here:</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter email">
+        </div>
+        <div class ="text-right">
+            <a href="Login"<button type="cancel" class="btn btn-default">Cancel</button> </a>
+            
+            <a href="forgotpwd_confirmation"<button type="submit" class="btn btn-default">Submit</button> </a>
+        </div>
+    </form>
+
+</div>
