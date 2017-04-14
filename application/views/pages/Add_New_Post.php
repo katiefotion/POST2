4b68eaae6824709eb264bf74c06b74bbd0baf9d9
rain <rain2288@gmail.com>
2017-04-14 16:42:33 -0700

    Changed Button Size

--- application/views/pages/Add_New_Post.php
+++ application/views/pages/Add_New_Post.php
@@ -131,14 +131,16 @@
         </div>
     </div>
     <div class = "row">
-        <div class = "col-md-5"> </div>
-        <div class = "col-md-2"> 
+        <div class = "col-md-4"> </div>
+            <div class = "col-md-2"> 
             <a href="./" class="btn btn-danger btn-block" role="button">Cancel</a>
- 
+        </div>
+        <div class = "col-md-2"> 
+
             <button type="submit" class="btn btn-success btn-block">Add</button>
  
         </div>
-        <div class = "col-md-5"> </div>
+        <div class = "col-md-4"> </div>
     </div>
 </div>
 
