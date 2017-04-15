5a684d9b8d2a5c4ab0e1dc21c358772e5925cd8e
Ronald Rieger <rrieger@email.sfsu.edu>
2017-04-14 17:31:01 -0700

    added font-awesome  to the site

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,6 +13,7 @@ $this->load->helper('html');
 
         <!-- Bootstrap -->
         <link href="<?= base_url('assets/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+        <link href="<?= base_url('assets/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
         <link href="<?= base_url('assets/style.css'); ?>" rel="stylesheet">
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
