e0485b42d010e9ff309272ccbb12afe143c922b8
priya <kkrishna@mail.sfsu.edu>
2017-04-16 13:44:30 -0700

    changed register_confirmation.php

---
+++ application/views/pages/register_confirmation.php
@@ -0,0 +1,19 @@
+
+<div class="container-fluid">
+    <div class="text-center">
+        <h1>Thank you for signing up with GatorSell!</h1> 
+        <p>Please check your email for confirmation code to activate your account.</p>
+    </div>
+    <form>
+        <div class="form-group">
+            <label for="code">Enter your confirmation code:</label>
+            <input type="code" class="form-control" id="email" placeholder="confimation code">
+        </div>
+        <div class ="text-right">
+            <a href="Login"<button type="cancel" class="btn btn-default">Cancel</button> </a>
+            
+            <a href="home"<button type="submit" class="btn btn-default">Submit</button> </a>
+        </div>
+    </form>
+</div>
+
