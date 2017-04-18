20ca24374df0d67f903c925d651d40f49015f108
Ronald Rieger <rrieger@email.sfsu.edu>
2017-04-18 11:05:13 -0700

    fixed gator helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('gator','url');
+$autoload['helper'] = array('Gator','url');
 
 /*
 | -------------------------------------------------------------------
